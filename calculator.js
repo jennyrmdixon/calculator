03e1e8009ae63ee7f82807518bdc0490b96ab5e4
Jenny Dixon <jennyrmdixon@gmail.com>
2022-11-27 11:40:30 -0800

    Add functions for basic add, subtrac, divide, and multiply operations

---
+++ calculator.js
@@ -0,0 +1,22 @@
+//Define basic functions
+
+const add = function(x, y) {
+    return x + y;
+      
+  };
+  
+  const subtract = function(x, y) {
+    return x - y;
+  };
+  
+  
+  const multiply = function(x, y) {
+    return x * y;
+  };
+
+  const divide = function(x, y) {
+    return x / y;
+  };
+  
+  
+ 
