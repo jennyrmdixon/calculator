c63430cb7d271527704b803b54f409b8542cc837
Jenny Dixon <jennyrmdixon@gmail.com>
2022-11-28 20:26:12 -0800

    Add HTML skeleton and operator function

--- calculator.js
+++ calculator.js
@@ -1,22 +1,30 @@
 //Define basic functions
 
-const add = function(x, y) {
-    return x + y;
+const add = function(num1, num2) {
+    return num1 + num2;
       
   };
   
-  const subtract = function(x, y) {
-    return x - y;
+  const subtract = function(num1, num2) {
+    return num1 - num2;
   };
   
   
-  const multiply = function(x, y) {
-    return x * y;
+  const multiply = function(num1, num2) {
+    return num1 * num2;
   };
 
-  const divide = function(x, y) {
-    return x / y;
+  const divide = function(num1, num2) {
+    return num1 / num2;
   };
   
-  
- 
+  let operate = function (operator, num1, num2) {
+    switch (operator) {
+        case "+": return add(num1, num2);
+        case "-": return subtract(num1, num2);
+        case "*": return multiply(num1, num2);
+        case "/": return divide(num1, num2);
+       default: return "error";
+    }
+  }
+
